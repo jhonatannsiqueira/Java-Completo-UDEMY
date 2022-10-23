5430f8c5eeefe90cb89493c53ad790edce3f9607
Jhonatan <john-nunes@outlook.com>
2022-10-23 19:28:22 -0300

    Conclusão de Problema com POO e aplicação de toString

--- V - Introdução a POO/entities/Produto.java
+++ V - Introdução a POO/entities/Produto.java
@@ -10,13 +10,20 @@ public class Produto {
 		return preco * quantidadeEstoque;
 	}
 	
-	// int quantidadeEstoque refere-se ao Parametro da Fun��o
-	public void addProduto(int quantidadeEstoque) { // Essa opera��o n�o retorna nada, apenas altera o valor do Atributo quanidadeEstoque
-		this.quantidadeEstoque += quantidadeEstoque; // this � uma palavra reservada que significa uma autoreferencia ao Objeto
+	// int quantidadeEstoque refere-se ao Parametro da Função
+	public void addProduto(int quantidadeEstoque) { // Essa operação não retorna nada, apenas altera o valor do Atributo quanidadeEstoque
+		this.quantidadeEstoque += quantidadeEstoque; // this é uma palavra reservada que significa uma autoreferencia ao Objeto, não o confundindo com o Parametro utilizado na Função
 	}
 	
 	public void removerProduto(int quantidadeEstoque) {
 		this.quantidadeEstoque -= quantidadeEstoque;
+	}
+	
+	public String toString() { //toString é nativo da Object, no caso eu o usei como uma Mascara de Formatação
+		return String.format("Console: %s%n", nome)
+				+ String.format("Custa R$%.2f%n", preco)
+				+ String.format("Quantidade em Estoque: %d%n", quantidadeEstoque)
+				+ String.format("$$$ deste Produto em Estoque: R$%.2f%n", valorTotalEmEstoque());
 	}
 
 }
