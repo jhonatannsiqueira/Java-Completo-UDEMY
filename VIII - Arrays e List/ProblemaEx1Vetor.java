366512906270fb53f5b90364da80ba285d9b9167
Jhonatan <john-nunes@outlook.com>
2023-02-22 22:06:40 -0300

    Introdução a Vetores/Arrays - Exercício Prático I

---
+++ VIII - Arrays e List/ProblemaEx1Vetor.java
@@ -0,0 +1,39 @@
+package vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaEx1Vetor {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Fazer um programa para ler um número inteiro N e a altura de N pessoas.
+		 * Armazene as N alturas em um vetor. Em seguida, mostrar a altura média dessas
+		 * pessoas.
+		 */
+
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Informe um N° de Pessoas: ");
+		int n = leia.nextInt();
+		double[] vetor = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Informe a Altura: ");
+			vetor[i] = leia.nextDouble();
+		}
+
+		double soma = 0.0;
+		for (int i = 0; i < n; i++) {
+			soma += vetor[i];
+		}
+
+		double media = soma / n;
+
+		System.out.printf("A Média de Altura é: %.2f%n", media);
+
+	}
+
+}
